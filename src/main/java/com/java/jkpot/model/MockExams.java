83c64ceef53c49b499cec013d63e78adbac5a8be
ujju79sharma <ujjwalbennington@gmail.com>
2021-04-17 20:49:44 +0530

    changes

--- src/main/java/com/java/jkpot/model/MockExams.java
+++ src/main/java/com/java/jkpot/model/MockExams.java
@@ -3,10 +3,7 @@ package com.java.jkpot.model;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import com.poiji.annotation.ExcelCellName;
 
 @Document(collection = "mock_exams")
 public class MockExams {
@@ -14,35 +11,12 @@ public class MockExams {
 	@Id
 	private int mockExamId;
 	private int examId;
-	@ExcelCellName("Section") 
 	private String sectionName;
-	@ExcelCellName("Sno") 
 	private int sectionQuestionNo;
-	@ExcelCellName("Questions")
 	private String question;
-	@ExcelCellName("Answer")
 	private String answer;
-	
-	@Transient
-	@ExcelCellName("Option-A")
-	private String optionA;
-	@Transient
-	@ExcelCellName("Option-B")
-	private String optionB;
-	@Transient
-	@ExcelCellName("Option-C")
-	private String optionC;
-	@Transient
-	@ExcelCellName("Option-D")
-	private String optionD;
-	
-	@ExcelCellName("Name")
 	private List<String> options;
-	@ExcelCellName("Image-Added")
-	private String imageOption;
-	
 	private boolean imageAdded;
-	@ExcelCellName("Image")
 	private String imageName;
 
 	public int getMockExamId() {
@@ -117,51 +91,10 @@ public class MockExams {
 		this.imageName = imageName;
 	}
 
-	public String getOptionA() {
-		return optionA;
-	}
-
-	public void setOptionA(String optionA) {
-		this.optionA = optionA;
-	}
-
-	public String getOptionB() {
-		return optionB;
-	}
-
-	public void setOptionB(String optionB) {
-		this.optionB = optionB;
-	}
-
-	public String getOptionC() {
-		return optionC;
-	}
-
-	public void setOptionC(String optionC) {
-		this.optionC = optionC;
-	}
-
-	public String getOptionD() {
-		return optionD;
-	}
-
-	public void setOptionD(String optionD) {
-		this.optionD = optionD;
-	}
-
-	public String getImageOption() {
-		return imageOption;
-	}
-
-	public void setImageOption(String imageOption) {
-		this.imageOption = imageOption;
-	}
-
 	@Override
 	public String toString() {
 		return "MockExams [mockExamId=" + mockExamId + ", examId=" + examId + ", sectionName=" + sectionName
-				+ ", sectionQuestionNo=" + sectionQuestionNo + ", question=" + question + ", answer=" + answer
-				+ ", optionA=" + optionA + ", optionB=" + optionB + ", optionC=" + optionC + ", optionD=" + optionD
-				+ ", options=" + options + ", imageAdded=" + imageAdded + ", imageName=" + imageName + "]";
+				+ ", sectionQuestionNo=" + sectionQuestionNo + ", question=" + question + ", options=" + options
+				+ ", imageAdded=" + imageAdded + ", imageName=" + imageName + "]";
 	}
 }
