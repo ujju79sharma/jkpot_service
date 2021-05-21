ec6e7e86ba8e9fc31b27562832e64d496f027b5e
ujju79sharma <ujjwalbennington@gmail.com>
2021-05-21 13:10:03 +0530

    added mongo cluser db

--- src/main/java/com/java/jkpot/JkpotApplication.java
+++ src/main/java/com/java/jkpot/JkpotApplication.java
@@ -2,11 +2,25 @@ package com.java.jkpot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 @SpringBootApplication
 public class JkpotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JkpotApplication.class, args);
+	}
+
+	@Bean
+	public MongoClient getMongoConnectivity() {
+
+		String url1 = "mongodb+srv://jkpot:jkpot@jkpot.uem5j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+		MongoClient mongoClient = MongoClients.create(url1);
+
+		return mongoClient;
 	}
 }
